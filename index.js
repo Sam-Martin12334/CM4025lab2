68fb25bda8ae82fa7eeb36fd0c7130c2f4ca6546
SAM MARTIN (1804372) <s.martin8@rgu.ac.uk>
2022-01-25 15:54:19 +0000

    add all

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var http = require("http");
 const PORT = process.env.PORT || 3000;
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-
-    response.end('Hello World\n');
-}).listen(8080);
-
-
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
-   });  
+   });
+
+   
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+
+    response.end('Hello World\n');
+}).listen(8080)
+
+
+
+
+   
+
+console.log('Server running at http://127.0.0.1:8080/')
