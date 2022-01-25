a35d669c402840849d4bcebf60fa36c27105143d
SAM MARTIN (1804372) <s.martin8@rgu.ac.uk>
2022-01-25 15:56:43 +0000

    add all

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var http = require("http");
 const PORT = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+
+
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
    });
 
-   
+
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'})
 
