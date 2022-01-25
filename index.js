c321b4f86f53699340bfa2159c40724e66f36e13
SAM MARTIN (1804372) <s.martin8@rgu.ac.uk>
2022-01-25 16:00:28 +0000

    add all

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 var http = require("http");
 const PORT = process.env.PORT || 3000;
-var express = require('express');
-var app = express();
 
-
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-   });
 
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'})
 
     response.end('Hello World\n');
-}).listen(8080)
+}).listen(PORT)
 
 
 
 
    
 
-console.log('Server running at http://127.0.0.1:8080/')
+console.log('Server running at http://127.0.0.1: ${ PORT }/')
